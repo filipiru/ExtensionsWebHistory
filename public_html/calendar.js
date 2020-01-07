816ab1e0ecdf7945ee76028b2f6446978af81aa8
filipiru <filipiru@workstation>
2020-01-07 10:51:38 +0100

    browser history page complete

---
+++ public_html/calendar.js
@@ -0,0 +1,38 @@
+/* global browser */
+
+
+
+/*
+ var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+ var dayNames = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+ 
+ var calendar = document.createElement("table");
+ calendar.className = "table table-sm table-dark";
+ var month = document.createElement("tr");
+ var th = document.createElement("th");
+ th.innerText = "2020";
+ month.appendChild(th);
+ 
+ for (var d = 1; d < 32; d++) {
+ var day = document.createElement("th");
+ day.innerText = d;
+ month.appendChild(day);
+ 
+ }
+ calendar.appendChild(month);
+ 
+ for (var m = 0; m < 12; m++) {
+ var month = document.createElement("tr");
+ var th = document.createElement("th");
+ th.innerText = monthNames[m];
+ month.appendChild(th);
+ 
+ for (var d = 1; d < 32; d++) {
+ var day = document.createElement("td");
+ day.innerText = "x";
+ month.appendChild(day);
+ 
+ }
+ calendar.appendChild(month);
+ }
+ document.getElementById('calendar').appendChild(calendar);*/
--- public_html/sites.js
+++ public_html/sites.js
@@ -1,5 +1,6 @@
 /* global browser */
 
+/* * TOP PAGESITES * */
 browser.topSites.get()
 .then((sites) => {
 	var div = document.getElementById('site-list');
@@ -11,28 +12,128 @@ browser.topSites.get()
 	let ul = document.createElement('ul');
 	ul.className = 'list-group';
 	for (let site of sites) {
-		let li = document.createElement('li');
-		li.className = 'list-group-item';
-		let a = document.createElement('a');
-		a.href = site.url;
-		a.innerText = site.title || site.url;
-		li.appendChild(a);
-		ul.appendChild(li);
+		if (site.title) {
+			let li = document.createElement('li');
+			li.className = 'list-group-item text-success';
+			var url = new URL(site.url);
+			var pathName = url.pathname.split('/');
+			pathName = pathName.join(' > ');
+			li.innerText = urlPreprocesor(url.hostname) + " " + pathName + " ";
+			let a = document.createElement('a');
+			a.href = site.url;
+
+			//a.innerText = site.title || site.url;
+			a.innerText = site.title;
+			let br = document.createElement('br');
+			li.appendChild(br);
+			li.appendChild(a);
+			ul.appendChild(li);
+		} else {
+			let li = document.createElement('li');
+			li.className = 'list-group-item text-danger';
+			var url = new URL(site.url);
+			var pathName = url.pathname.split('/');
+			pathName = pathName.join(' > ');
+			li.innerText = urlPreprocesor(url.hostname) + " " + pathName + " ";
+			let a = document.createElement('a');
+			a.href = site.url;
+
+			a.innerText = "link (no title)";
+			li.appendChild(a);
+			ul.appendChild(li);
+		}
 	}
 
 	div.appendChild(ul);
 });
 
+/* * LAST HISTORY VISITED * */
+browser.history.search({
+	text: "",
+	startTime: 0
+}).then((histories) => {
+	if (histories.length) {
+		//console.log(histories.length);
+		ShowHistory(histories);
+	}
+});
+
+function ShowHistory(sites) {
+	//console.log(sites);
+
+	var div = document.getElementById('history-items');
+	if (!sites.length) {
+		div.innerText = 'No sites returned from the topSites API.';
+		return;
+	}
+
+	let ul = document.createElement('ul');
+	ul.className = 'list-group';
+	for (let site of sites) {
+		if (site.title) {
+			let li = document.createElement('li');
+			li.className = 'list-group-item text-success';
+			var url = new URL(site.url);
+			var pathName = url.pathname.split('/');
+			pathName = pathName.join(' > ');
+			li.innerText = urlPreprocesor(url.hostname) + " " + pathName + " ";
+			let a = document.createElement('a');
+			a.href = site.url;
+
+			//a.innerText = site.title || site.url;
+			a.innerText = site.title;
+			let br = document.createElement('br');
+			li.appendChild(br);
+			li.appendChild(a);
+			ul.appendChild(li);
+		} else {
+			let li = document.createElement('li');
+			li.className = 'list-group-item text-danger';
+			var url = new URL(site.url);
+			var pathName = url.pathname.split('/');
+			pathName = pathName.join(' > ');
+			li.innerText = urlPreprocesor(url.hostname) + " " + pathName + " ";
+			let a = document.createElement('a');
+			a.href = site.url;
+
+			a.innerText = "link (no title)";
+			li.appendChild(a);
+			ul.appendChild(li);
+		}
+	}
+
+	div.appendChild(ul);
+}
+
+function urlPreprocesor(urlHostname) {
+	var levelsDomain = urlHostname.split('.');
+	if (levelsDomain.length < 2) {
+		return urlHostname;
+	}
+	var preprocesorDomain = levelsDomain.reverse();
+	var tld = preprocesorDomain.shift();
+	var iild = preprocesorDomain.shift();
+	iild = iild.toUpperCase();
+	if (!preprocesorDomain.length) {
+		return iild + " (" + tld + ")";
+	}
+	//console.log(preprocesorDomain.join('.'));
+
+	return iild + "." + preprocesorDomain.join('.') + " (" + tld + ")";
+}
+
+/* * LAST VISITED ALL HISTORY * */
+
 function gotVisits(visits) {
 	//console.log("Visit count: " + visits.length);
 	document.getElementById('last-visit-url').innerText += " ( Visit count: " + visits.length + " )";
 	let ul = document.createElement('ul');
-	ul.className = '';
+	ul.className = 'list-group';
 
 	for (visit of visits) {
 		//console.log(visit.visitTime);
 		let li = document.createElement('li');
-		li.className = '';
+		li.className = 'list-group-item text-warning';
 		li.innerText = visit.visitTime || visit.visitTime;
 		ul.appendChild(li);
 	}
